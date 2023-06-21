2de33e97bc00f833eb9f1225751cc2026086456c
CorrectACHU <itdeveloper.yakimuk@gmail.com>
2023-06-21 16:07:53 +0200

    Changed code: -The abillity to retrieve photos from the back-end was added

--- carInsuranceBackEnd/src/main/java/com/yakvel/carInsuranceBackEnd/CarInsuranceBackEndApplication.java
+++ carInsuranceBackEnd/src/main/java/com/yakvel/carInsuranceBackEnd/CarInsuranceBackEndApplication.java
@@ -2,9 +2,22 @@ package com.yakvel.carInsuranceBackEnd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
-public class CarInsuranceBackEndApplication {
+public class CarInsuranceBackEndApplication implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        // Register resource handler for images
+        registry.addResourceHandler("/images/**").addResourceLocations("classpath:/static/photo/")
+                .setCacheControl(CacheControl.maxAge(2, TimeUnit.HOURS).cachePublic());
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CarInsuranceBackEndApplication.class, args);
