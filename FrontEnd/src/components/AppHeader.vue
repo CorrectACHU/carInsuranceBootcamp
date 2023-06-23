e8f6077e841ab524f1321035f429b6b196905b21
CorrectACHU <itdeveloper.yakimuk@gmail.com>
2023-06-23 11:00:55 +0200

    Changed code: -Added UserMainPage on the front side

--- FrontEnd/src/components/AppHeader.vue
+++ FrontEnd/src/components/AppHeader.vue
@@ -1,10 +1,25 @@
 <template>
   <header>
-    <v-toolbar class="toolbarSize" color="blue"></v-toolbar>
+    <v-toolbar class="toolbarSize" color="blue">
+      <v-btn 
+      class=""
+      variant="text"
+      @click="logout">
+        LogOut
+      </v-btn>
+    </v-toolbar>
   </header>
 </template>
 
 <script setup lang="ts">
+import router from '@/router';
+
+
+const logout = () => {
+  document.cookie = 'token'+'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+  router.push('/login')
+}
+
 </script>
 
 <style scoped>
---
+++ FrontEnd/src/views/user/UserMainPage.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <AppHeader />
+    <div class="d-flex align-start flex-wrap justify-space-around bodySize bg-light-blue-lighten-5">
+      <v-list class="bg-light-blue-lighten-5" v-for="item in fetchData" :key="item.id">
+        <v-card
+          minWidth=30dvw
+          minHeight=20dvh
+          :title=item.ticketOwner.contactInfo?.firstName
+          :subtitle=item.dateOfIncident
+          :text=item.ticketStatus
+          >
+        </v-card>
+      </v-list>
+    </div>
+    <AppFooter />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted, reactive } from 'vue'
+import AppHeader from '@/components/AppHeader.vue'
+import AppFooter from '@/components/AppFooter.vue'
+import router from '@/router'
+import { type CardInGeneral } from '@/stores/modelInterfaces'
+
+const getToken = () => {
+  const cookies = document.cookie
+  const cookiesArr = cookies.split(';')
+  const token = cookiesArr.find((cookie) => cookie.includes('token'))
+  if (token) {
+    return token.replace('token=', '')
+  }
+  return 'null'
+}
+
+const token = getToken()
+const fetchData: CardInGeneral[] = reactive([])
+const getListOfTickets = async () => {
+  try {
+    const response = await fetch('http://localhost:8080/api/v1/user/tickets', {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`
+      }
+    })
+    if (response.status === 401) {
+      const message = 'Your token was expired'
+      router.push('/login')
+    }
+    const data = await response.json()
+    data.forEach((ticket: CardInGeneral) => {
+      fetchData.push(ticket)
+    })
+  } catch (error) {
+    throw new Error('ssss')
+  }
+}
+
+onMounted(() => {
+  getListOfTickets()
+})
+</script>
+
+<style scoped>
+.bodySize {
+  min-height: 80dvh;
+}
+</style>
